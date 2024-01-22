66af5ed55bb33eb6cd5f14ef144e365b9752c979
Eren Erol <60065768+Eroyun@users.noreply.github.com>
2024-01-22 23:54:21 +0300

    Create a flask app that uses open ai

--- main.py
+++ main.py
@@ -1,36 +1,45 @@
+from flask import Flask, request, jsonify
 from dotenv import dotenv_values
-
-from langchain.prompts.chat import (
-    SystemMessagePromptTemplate,
-    MessagesPlaceholder,
-    HumanMessagePromptTemplate,
-    ChatPromptTemplate
-)
+from langchain.prompts.chat import SystemMessagePromptTemplate, MessagesPlaceholder, HumanMessagePromptTemplate, ChatPromptTemplate
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import LLMChain
 from langchain_openai import ChatOpenAI
+from langchain_community.callbacks import get_openai_callback
 
-env = dotenv_values(".env")
+app = Flask(__name__)
 
-llm = ChatOpenAI(
-    temperature=0, openai_api_key=env['OPENAI_KEY'])
 
-prompt = ChatPromptTemplate(
-    messages=[
-        SystemMessagePromptTemplate.from_template(
-            "You are a nice chatbot having a conversation with a human."
-        ),
-        # The `variable_name` here is what must align with memory
-        MessagesPlaceholder(variable_name="chat_history"),
-        HumanMessagePromptTemplate.from_template("{question}"),
-    ]
-)
+@app.route('/conversation', methods=['POST'])
+def conversation():
+    response = ""
+    data = request.get_json()
 
-memory = ConversationBufferMemory(
-    memory_key="chat_history", return_messages=True)
+    env = dotenv_values(".env")
 
-conversation = LLMChain(llm=llm, prompt=prompt, verbose=True, memory=memory)
+    with get_openai_callback() as cb:
+        chat_openai = ChatOpenAI(
+            temperature=0, openai_api_key=env['OPENAI_KEY'])
 
-print(conversation({"question": "my name is Eren"}))
+        prompt = ChatPromptTemplate(
+            messages=[
+                SystemMessagePromptTemplate.from_template(
+                    "You are a nice chatbot having a conversation with a human."),
+                MessagesPlaceholder(variable_name="chat_history"),
+                HumanMessagePromptTemplate.from_template("{question}"),
+            ]
+        )
 
-print(conversation({"question": "what is my name?"}))
+        memory = ConversationBufferMemory(
+            memory_key="chat_history", return_messages=True)
+
+        conversation = LLMChain(
+            llm=chat_openai, prompt=prompt, verbose=True, memory=memory)
+
+        response = conversation({"question": data["message"]})
+        print(response)
+        print(cb)
+    return jsonify({"response": response["text"]})
+
+
+if __name__ == '__main__':
+    app.run()
