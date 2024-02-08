b821f84909a0800162a33abe92ae5b06ecd266af
Melinda Ghertiu <157207717+melindaGh@users.noreply.github.com>
2024-02-08 23:27:37 +0300

    Welcome Page Part1 Version 5

--- composable/app/pages/RecipesHomePage.tsx
+++ composable/app/pages/RecipesHomePage.tsx
@@ -1,12 +1,12 @@
 import {SafeAreaView, View} from "react-native";
 import React from "react";
-import RecipesHomePageHeader from "../components/RecipesHomePageHeader"
 import RecipesSearch from "../components/RecipesSearch";
 import Filter from "../components/RecipesFilter";
 import SuggestedRecipesCard from "../components/SuggestedRecipesCard";
 import {colors} from "../utils/constants";
 import {homepageStyles} from "../theme/components/theme";
 import RecipesCategories from "../components/RecipesCategories";
+import RecipesHomePageHeader from "../components/RecipesHomePageHeader"
 const RecipesHomePage = () => {
 
     const handleSearch = (searchText : string) => {
@@ -16,14 +16,16 @@ const RecipesHomePage = () => {
     return (
         <SafeAreaView
             style={{
-            flex: 1,
-            backgroundColor: colors.background
+                flex:1,
+            backgroundColor: "#fffff"
         }}>
-            <RecipesHomePageHeader headerText={"What would you like to cook?"}/>
-            
-            <View style={homepageStyles.container}>
+            <View>
+            <RecipesHomePageHeader headerText={"What2Cook"}/>
+            <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', paddingHorizontal: 8, paddingBottom: 2 }}>
+            <View style={{ flexDirection: 'row', flex: 1, alignItems: 'center', padding: 7}}>
+                {/* <Filter /> */}
                 <RecipesSearch onSearch={handleSearch} />
-                <Filter />
+            </View></View>
             </View>
             <RecipesCategories/>
             <SuggestedRecipesCard/>
