6611da232897007ebdfe82f26fe80aefc8f7be50
Eren Erol <60065768+Eroyun@users.noreply.github.com>
2024-02-11 12:46:40 +0300

    Update App.js

--- composable/App.js
+++ composable/App.js
@@ -1,25 +1,26 @@
 import React from "react";
-import {StyleSheet, Text, View} from "react-native";
-import {NavigationContainer} from "@react-navigation/native";
-import {createNativeStackNavigator} from "@react-navigation/native-stack";
-import RecipesHomePage from "../pages/RecipesHomePage";
-import RecipeDetailsPage from "../pages/RecipeDetailsPage";
-import {registerRootComponent} from "expo";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import RecipesHomePage from "./app/pages/RecipesHomePage";
+import RecipeDetailsPage from "./app/pages/RecipeDetailsPage";
+import { registerRootComponent } from "expo";
 
 const Stack = createNativeStackNavigator();
 
 const AppNavigator = () => {
-    return (
-        <NavigationContainer>
-            <Stack.Navigator
-                screenOptions={{
-                headerShown: false
-            }}>
-                <Stack.Screen name="Welcome" component={RecipesHomePage}/>
-                <Stack.Screen name="RecipeDetails" component={RecipeDetailsPage}/>
-            </Stack.Navigator>
-        </NavigationContainer>
-    );
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="Welcome" component={RecipesHomePage} />
+        <Stack.Screen name="RecipeDetails" component={RecipeDetailsPage} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 
 export default registerRootComponent(AppNavigator);
