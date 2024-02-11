76672330248c86f97b9f91cafc8411b18fd4df20
Eren Erol <60065768+Eroyun@users.noreply.github.com>
2024-02-11 12:43:14 +0300

    Move App.js

--- composable/tailwind.config.js
+++ composable/tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["../composable/app/**/*.{js,jsx,ts,tsx}"],
+  content: ["../composable/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
