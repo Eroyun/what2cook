968cff56103d89159ea2f3e617e1e34a1859b067
Eren Erol <60065768+Eroyun@users.noreply.github.com>
2024-03-03 21:38:03 +0300

    Index payload

---
+++ qdrant/index_payload.py
@@ -0,0 +1,19 @@
+from qdrant_client import QdrantClient
+import os
+
+client = QdrantClient(
+    url=os.environ["QDRANT_URL"],
+    api_key=os.environ["QDRANT_KEY"],
+)
+
+client.create_payload_index(
+    collection_name="recipes",
+    field_name="TotalTime",
+    field_schema="integer"
+)
+
+client.create_payload_index(
+    collection_name="recipes",
+    field_name="RecipeIngredientParts",
+    field_schema="text"
+)
--- qdrant/update_collection.py
+++ qdrant/update_collection.py
@@ -1,8 +1,5 @@
 from qdrant_client import QdrantClient, models
-from sentence_transformers import SentenceTransformer
 import os
-
-encoder = SentenceTransformer("all-MiniLM-L6-v2")
 
 client = QdrantClient(
     url=os.environ["QDRANT_URL"],
